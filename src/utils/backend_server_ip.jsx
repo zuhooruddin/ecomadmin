62dbb77419c009c42a33a8780d842858cd80656d
zuhooruddin <100421830+zuhooruddin@users.noreply.github.com>
2023-12-25 13:40:59 +0300

    Update backend_server_ip.jsx

--- src/utils/backend_server_ip.jsx
+++ src/utils/backend_server_ip.jsx
@@ -1,3 +1,3 @@
 // export const server_ip = 'https://idrisbookbank-dev-server.inara.tech';
-export const server_ip = 'http://127.0.0.1:8000/';
+export const server_ip = 'http://aldanabeauty.com:8000/';
 
